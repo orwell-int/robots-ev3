e08173fa57a15009165cf24074d9a62d74d7bf2b
Michael Ludmann <miludmann@gmail.com>
2017-04-16 16:15:42 +0200

    Simplify Cli.java

--- robots-ev3-module/src/main/java/utils/Cli.java
+++ robots-ev3-module/src/main/java/utils/Cli.java
@@ -11,23 +11,26 @@ import java.util.Arrays;
 
 public class Cli {
     private final static Logger logback = LoggerFactory.getLogger(Cli.class);
+    private static final String COLOUR_INI_FILE_OPTION = "cf";
+    private static final String ROBOT_INI_FILE_OPTION = "rf";
+    private static final String HELP_OPTION = "h";
     private final Options options = new Options();
     private String[] args = null;
 
     public Cli(final String[] args) {
         this.args = args;
 
-        final Option optionHelp = new Option("h", "help", false, "shows help and exits program.");
+        final Option optionHelp = new Option(HELP_OPTION, "help", false, "shows help and exits program.");
         options.addOption(optionHelp);
 
         // Add a new optionGroup to make --file
-        final Option optionRobotIniFile = new Option("rf", "robotfile", true, "filepath for external robot configuration file");
+        final Option optionRobotIniFile = new Option(ROBOT_INI_FILE_OPTION, "robotfile", true, "filepath for external robot configuration file");
         final OptionGroup optionGroup = new OptionGroup();
         optionGroup.setRequired(false);
         optionGroup.addOption(optionRobotIniFile);
         options.addOptionGroup(optionGroup);
 
-        final Option optionColourIniFile = new Option("cf", "colourfile", true, "filepath for external colour configuration file");
+        final Option optionColourIniFile = new Option(COLOUR_INI_FILE_OPTION, "colourfile", true, "filepath for external colour configuration file");
         final OptionGroup optionGroup2 = new OptionGroup();
         optionGroup2.setRequired(true);
         optionGroup2.addOption(optionColourIniFile);
@@ -46,17 +49,19 @@ public class Cli {
         try {
             cmd = parser.parse(options, args);
 
-            if (cmd.hasOption("h"))
-                return help();
+            if (cmd.hasOption(HELP_OPTION)) {
+                help();
+                return null;
+            }
 
             String robotIniFileName = "";
-            if (cmd.hasOption("rf")) {
-                robotIniFileName = cmd.getOptionValue("rf");
+            if (cmd.hasOption(ROBOT_INI_FILE_OPTION)) {
+                robotIniFileName = cmd.getOptionValue(ROBOT_INI_FILE_OPTION);
             }
 
             String colourConfigIniFileName = "";
-            if (cmd.hasOption("cf")) {
-                colourConfigIniFileName = cmd.getOptionValue("cf");
+            if (cmd.hasOption(COLOUR_INI_FILE_OPTION)) {
+                colourConfigIniFileName = cmd.getOptionValue(COLOUR_INI_FILE_OPTION);
             }
 
             IniFiles iniFiles = new IniFiles(robotIniFileName, colourConfigIniFileName);
@@ -65,27 +70,24 @@ public class Cli {
                 return iniFiles;
             }
 
-            if (cmd.hasOption("rf") && cmd.hasOption("cf")) {
+            if (cmd.hasOption(ROBOT_INI_FILE_OPTION) && cmd.hasOption(COLOUR_INI_FILE_OPTION)) {
                 logback.warn(iniFiles.toString());
-                return help();
             } else if (args.length > 0) {
                 logback.warn("There is an unknown parameter among the following ones: " + Arrays.toString(args));
-                return help();
-            } else {
-                return help();
             }
 
         } catch (final ParseException e) {
             logback.error("Failed to parse command line properties", e);
-            return help();
         }
+
+        help();
+        return null;
     }
 
-    private IniFiles help() {
+    private void help() {
         // This prints out some help
         final HelpFormatter formatter = new HelpFormatter();
 
         formatter.printHelp("-f filepath", options);
-        return null;
     }
 }
